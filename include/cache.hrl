6b4584fc24b08e2856b41daede3aee129e9f6ce4
Matthias Horn <matthiash@gmail.com>
2012-01-08 18:38:30 +0000

    adds purge macro

--- include/cache.hrl
+++ include/cache.hrl
@@ -1,3 +1,10 @@
--define(cache(K,Value), ?cache_({?MODULE,K},Value)).
--define(cache(K1,K2,Value), ?cache_({?MODULE,K1,K2},Value)).
+
 -define(cache_(K,Value), etermcache:get(K,fun () -> Value end)).
+-define(purge_(K), etermcache:purge(K)).
+
+-define(cache(K,Value), ?cache_({?MODULE,K},Value)).
+-define(purge(K), ?purge({?MODULE,K})).
+
+-define(cache(K1,K2,Value), ?cache_({?MODULE,K1,K2},Value)).
+-define(purge(K1,K2), ?purge({?MODULE,K1,K2})).
+
