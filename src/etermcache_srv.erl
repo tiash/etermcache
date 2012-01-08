408bfc6d26dc268dbf6e3116408d9e6ba9a3fe0c
Matthias Horn <matthiash@gmail.com>
2012-01-08 13:26:55 +0000

    cleanup to use etermcache_config and state using value_pt

--- src/etermcache_srv.erl
+++ src/etermcache_srv.erl
@@ -2,6 +2,24 @@
 
 -define(APP,etermcache).
 
+-compile({parse_transform,value_pt}).
+
+-field(max_age).
+-field(time_to_live).
+-field(sleep).
+-field({needs_flush,[{default,false}]}).
+
+-member({loop/0,[accessor]}).
+-member({adjust/0,[mutator]}).
+-member({check/0,[mutator]}).
+-member({run_flush/1,[mutator]}).
+-member({run_flush/2,[mutator]}).
+-member({run_flush/5,[mutator]}).
+-member({receiver/0,[mutator]}).
+-member({receiver/1,[mutator]}).
+-member({receiver_/1,[mutator]}).
+-member({start_flush/0,[mutator]}).
+
 -export([start_link/0]).
 -export([flush/0]).
 -export([get/2]).
@@ -10,151 +28,121 @@
 
 -export([loop/1]).
 
--define(B(A),A).
--define(K(A),?B(A*1024)).
--define(M(A),?K(A*1024)).
--define(ms(A),A*1000).
--define(sec(A),?ms(A*1000)).
--define(min(A),?sec(A*60)).
+
 
 -define(DATA,etermcache_data).
 -define(LOG,etermcache_log).
 
--record(config, { max_bytes=?M(64)
-                , max_entries=?K(512)
-                , hard_max_age=?sec(30)
-                , max_age
-                , hard_time_to_live=?sec(10)
-                , time_to_live
-                }).
-init() ->
-  ?DATA = ets:new(?DATA,[set,public,{keypos,1},named_table,
-      {read_concurrency,true},{write_concurrency,true}]),
-  ?LOG = ets:new(?LOG,[set,public,{keypos,1},named_table,
-      {write_concurrency,true}]),
-  ok.
+init() -> init(etermcache_config:max_age(),etermcache_config:time_to_live(),etermcache_config:flush_sleep()).
 
 ensure_tables() ->
   case ets:info(?DATA,named_table) of
-    true -> 
-      case ets:info(?LOG,named_table) of
-        true -> ok;
-        _ -> init()
-      end;
-    _ -> init()
+    true -> ok;
+    _ ->
+      ?DATA = ets:new(?DATA,[set,public,{keypos,1},named_table,
+        {read_concurrency,true},{write_concurrency,true}]),
+      ok
+  end,
+  case ets:info(?LOG,named_table) of
+    true -> ok;
+    _ ->
+      ?LOG = ets:new(?LOG,[set,public,{keypos,1},named_table,
+          {write_concurrency,true}]),
+      ok
   end.
-
-% ensure() ->
-  % application:start(?APP),
-  % case check() of
-    % false -> flush();
-    % _ -> ok
-  % end,
-  % ok.
 
 start_link() ->
-  {ok,erlang:spawn_link(fun () ->
+  {ok,proc_lib:spawn_link(fun () ->
       register(?MODULE,self()),
       ensure_tables(),
-      ?MODULE:loop(config())
+      ?MODULE:loop(init())
   end)}.
 
-quota(Config) ->
-  case ets:info(?DATA,size) of
-    undefined -> 1;
-    Size ->
-      Bytes = ets:info(?DATA,memory) + ets:info(?LOG,memory),
-      max(Size*1.0/Config#config.max_entries
-         ,Bytes*1.0/Config#config.max_bytes)
+quota() ->
+  case status_bytes_entries() of
+    undefined -> undefined;
+    {Bytes,Entries} -> quota(Bytes,Entries)
   end.
+quota(undefined,_Entries) -> undefined;
+quota(_Bytes,undefiend) -> undefined;
+quota(Bytes,Entries) ->
+  max(Entries*1.0/etermcache_config:max_entries()
+     ,Bytes*1.0/etermcache_config:max_bytes()).
 
-check() -> check(config()).
-check(Config) ->
-  Q = quota(Config),
-  Q=<1.0.
+start_flush() ->
+  sleep(0), needs_flush(true).
 
-config() -> 
-  Config0=#config{hard_max_age=MaxAge,hard_time_to_live=TimeToLive} = update(#config{}),
-  Config0#config{max_age=MaxAge,time_to_live=TimeToLive}.
-update(Config) -> update(application:get_all_env(?APP),Config).
-update([],Config) -> Config;
-update([O|Opts],Config) -> update(Opts,update(O,Config));
-update({max_age,A},Config) -> Config#config{hard_max_age=A};
-update({time_to_live,A},Config) -> Config#config{hard_time_to_live=A};
-update({max_bytes,A},Config) -> Config#config{max_bytes=A};
-update({max_entries,A},Config) -> Config#config{max_entries=A};
-update(_,Config) -> Config.
-
-adjust(Config=#config{ hard_max_age=HardMaxAge
-                     , max_age=MaxAge
-                     , hard_time_to_live=HardTimeToLive
-                     , time_to_live=TimeToLive
-                     }) ->
-  case quota(Config) of
-    1 -> Config;
-    Q -> 
+adjust() ->
+  case quota() of
+    1 -> ok;
+    Q ->
+      if Q>1 -> start_flush();
+         true -> ok
+      end,
       Q1 = max(0.9,Q),
       Q2 = max(0.8,Q*Q),
-      Config#config { max_age=min(HardMaxAge,round(MaxAge/Q1))
-                    , time_to_live=min(HardTimeToLive,round(TimeToLive/Q2))
-                    }
+      max_age(min(etermcache_config:max_age(),round(max_age()/Q1))),
+      time_to_live(min(etermcache_config:time_to_live(),round(time_to_live()/Q2)))
   end.
 
-loop(Config0) ->
-    Config1 = update(Config0),
-    Rate0 = case check(Config1) of true -> {100,1000}; _ -> immediate end,
-    Config2 = adjust(Config1),
-    Rate1 = updateRate(Config2#config.time_to_live,Rate0,?LOG,ets:first(?LOG)),
-    _Rate2 = updateRate(Config2#config.max_age,Rate1,?DATA,ets:first(?DATA)),
-    receive 
-      {time_to_live,Ref,Pid} -> Pid!{time_to_live,Ref,Config2#config.time_to_live};
-      {max_age,Ref,Pid} -> Pid!{max_age,Ref,Config2#config.max_age}
-    after 0 -> ok
+receiver() -> receiver(sleep()).
+receiver(Sleep) -> 
+  adjust(), receiver_(Sleep).
+receiver_(Sleep) ->
+  receive
+    {time_to_live,Ref,Pid} -> Pid!{time_to_live,Ref,time_to_live()}, receiver_(0);
+    {max_age,Ref,Pid} -> Pid!{max_age,Ref,max_age()}, receiver_(0);
+    {purge,Key} -> lpurge(Key), receiver_(0);
+    flush -> start_flush();
+    _ -> receiver_(0)
+  after Sleep -> ok
+  end.
+
+loop() ->
+    case needs_flush() of
+      true ->
+        receiver(etermcache_config:flush_delay());
+      _ ->
+        sleep(etermcache_config:flush_sleep()),
+        receiver(etermcache_config:flush_delay())
     end,
-    ?MODULE:loop(Config2).
+    needs_flush(false),
+    run_flush(?LOG),
+    receiver(sleep()),
+    adjust(),
+    run_flush(?DATA),
+    ?MODULE:loop(THIS).
+
+run_flush(Table) ->
+  run_flush(Table,ets:first(Table)).
+
+run_flush(?DATA,Key) ->
+  run_flush(etermcache_config:flush_block(),max_age(),tsNow(),?DATA,Key);
+run_flush(?LOG,Key) ->
+  run_flush(etermcache_config:flush_block(),time_to_live(),tsNow(),?LOG,Key).
+
+run_flush(_Count,_MaxAge,_Now,_Table,'$end_of_table') -> ok;
+run_flush(0,_MaxAge,_Now,Table,Key) ->
+  receiver(),
+  adjust(),
+  run_flush(Table,Key);
+run_flush(Count,MaxAge,Now,Table,Key) ->
+  case try {ok,ets:next(Table,Key)} catch error:badarg -> error end of
+    {ok,Next} ->
+      try
+        TS = tsPlus(ets:lookup_element(Table,Key,2),MaxAge),
+        if TS<Now -> lpurge(Key);
+           true -> ok
+        end
+      catch 
+        error:badarg -> ok
+      end,
+      run_flush(Count,MaxAge,Now,Table,Next);
+    _ -> needs_flush(true) %% Something went wrong with the table scan so ensure we do another scan ASAP
+  end.
 
 flush() ->
   ?MODULE!flush.
-
-flush(_TimeOut,Rate,_Count,_Now,_Table,'$end_of_table') -> Rate;
-flush(TimeOut,Rate,0,_Now,Table,Key) -> updateRate(TimeOut,Rate,Table,Key);
-flush(TimeOut,Rate,Count,Now,Table,Key) ->
-  Next = ets:next(Table,Key),
-  try
-    TS = tsPlus(ets:lookup_element(Table,Key,2),TimeOut),
-    if TS<Now -> lpurge(Key);
-       true -> ok
-    end
-  catch 
-    error:badarg -> ok
-  end,
-  flush(TimeOut,Rate,Count-1,Now,Table,Next).
-
-
-updateRate(TimeOut,ORate,Table,Key) ->
-  updateRate(TimeOut,ORate,sleep(ORate),Table,Key).
-updateRate(TimeOut,ORate,Sleep,Table,Key) ->
-  receive
-    purge -> lpurge(all);
-    {purge,Key} -> lpurge(Key);
-    {flush,NRate} ->
-      updateRate(TimeOut,fastest(ORate,NRate),0,Table,Key);
-    flush ->
-      updateRate(TimeOut,immediate,0,Table,Key)
-  after Sleep ->
-    flush(TimeOut,ORate,step(ORate),tsNow(),Table,Key)
-  end.
-
-sleep(immediate) -> 0;
-sleep({_,Sleep}) -> Sleep.
-step(immediate) -> 100;
-step({Step,_}) -> Step.
-fastest(immediate,_) -> immediate;
-fastest(_,immediate) -> immediate;
-fastest({A,B},{C,D}) -> 
-  if A*D > C*B -> {A,B};
-     true -> {C,D}
-  end.
 
 tsPlus({A,B,C},{X,Y,Z}) -> tsNorm({A+X,B+Y,C+Z});
 tsPlus(A,B) -> tsPlus(tsNorm(A),tsNorm(B)).
@@ -165,7 +153,7 @@ tsNorm({A0,B0,C0}) ->
   B2 = B1 rem 1000000,
   A1 = A0 + (B1 div 1000000),
   {A1,B2,C1};
-tsNorm(A) when is_integer(A) -> tsNorm({0,0,A}).
+tsNorm(A) when is_integer(A) -> tsNorm({0,A div 1000,(A rem 1000)*1000}).
 
 tsNow() -> tsNorm(now()).
 % tsNow(A) -> tsPlus(now(),A).
@@ -182,59 +170,74 @@ get(Key,Fun) ->
           Value
   end.
 
-purge() -> purge(all).
-purge(K) -> purge(K,[node()|nodes()]).
-purge(_K,[]) -> ok;
-purge(K,[N|Ns]) ->
-  spawn(N,?MODULE,lpurge,K),
-  purge(K,Ns).
+purge() -> 
+  lists:foreach(fun (N) -> {?MODULE,N} ! purge end, nodes()),
+  lpurge().
+  
+purge(K) ->
+  lists:foreach(fun (N) -> {?MODULE,N} !  {purge,K} end, nodes()),
+  lpurge(K).
 
-lpurge(all) ->
+lpurge() ->
   ets:delete_all_objects(?DATA),
-  ets:delete_all_objects(?LOG);
-lpurge([]) -> ok;
-lpurge([K|Ks]) -> lpurge(K), lpurge(Ks);
+  ets:delete_all_objects(?LOG).
 lpurge(K) ->
   ets:delete(?DATA,K),
   ets:delete(?LOG,K).
 
+einfo(Info) ->
+  case ets:info(?DATA,Info) of
+    undefined -> undefined;
+    A ->
+      case ets:info(?LOG,Info) of
+        undefined -> undefiend;
+        B -> {A,B}
+      end
+  end.
+
+status_bytes() ->
+  case einfo(memory) of
+    undefined -> undefined;
+    {A,B} -> A+B
+  end.
+status_entries() ->
+  case einfo(size) of
+    undefined -> undefined;
+    {A,B} -> max(A,B)
+  end.
+status_bytes_entries() ->
+  case status_bytes() of
+    undefined -> undefined;
+    A ->
+      case status_entries() of
+        undefined -> undefined;
+        B -> {A,B}
+      end
+  end.
 
 status() ->
   Ref = make_ref(),
   ?MODULE!{max_age,Ref,self()},
   ?MODULE!{time_to_live,Ref,self()},
+  case status_bytes_entries() of
+    {Bytes,Entries} ->
+      [ {bytes,Bytes}
+      , {entries,Entries}
+      , {quota,quota(Bytes,Entries)}
+      ];
+    undefined -> []
+  end
+  ++
   receive
-    {max_age,Ref,MaxAge} -> ok
-  after 5000 -> MaxAge = undefined end,
+    {max_age,Ref,MaxAge} -> [{max_age,MaxAge}]
+  after 5000 -> [] end
+  ++
   receive
-    {time_to_live,Ref,TimeToLive} -> ok
-  after 5000 -> TimeToLive = undefined end,
-  [ {bytes,status(bytes)}
-  , {entries,status(entries)}
-  , {quota,status(quota)}]
-  ++  case TimeToLive of
-        undefined -> [];
-        _ -> [{time_to_live,TimeToLive}]
-      end
-  ++  case MaxAge of
-        undefined -> [];
-        _ -> [{max_age,MaxAge}]
-      end.
+    {time_to_live,Ref,TimeToLive} -> [{time_to_live,TimeToLive}]
+  after 5000 -> [] end.
 
-status(bytes) ->
-  case {ets:info(?DATA,memory),ets:info(?LOG,memory)} of
-    {undefined,undefined} -> 0;
-    {A,undefined} -> A;
-    {undefined,A} -> A;
-    {A,B} -> A+B
-  end;
-status(entries) ->
-  case {ets:info(?DATA,size),ets:info(?LOG,size)} of
-    {undefined,undefined} -> 0;
-    {A,undefined} -> A;
-    {undefined,A} -> A;
-    {A,B} -> max(A,B)
-  end;
+status(bytes) -> status_bytes();
+status(entries) -> status_entries();
 status(time_to_live) ->
   Ref = make_ref(),
   ?MODULE ! {time_to_live,Ref,self()},
@@ -249,7 +252,7 @@ status(max_age) ->
     {max_age,Ref,MaxAge} -> MaxAge
   after 5000 -> undefined
   end;
-status(quota) -> quota(config()).
+status(quota) -> quota().
 
     
   
